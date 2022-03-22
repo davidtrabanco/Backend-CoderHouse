5aa466f246117beb9df885753c284cbf63286019
David Trabanco <davidtrabanco@gmail.com>
2022-03-21 23:22:22 -0300

    class 6 added

---
+++ Desafíos/test/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+const server = app.listen(8080,()=>console.log(`server up on port ${server.address().port}`))
+
+app.get("/",(req,res)=>{
+    res.send("hola!!")
+})
